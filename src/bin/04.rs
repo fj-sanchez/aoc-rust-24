54f5a0a125a201976a7b881ba3ae16e6032e5800
Fran Sanchez <francisco.sanchez@bosch.com>
2024-12-06 17:16:49 +0000

    Solution day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -2,7 +2,7 @@ advent_of_code::solution!(4);
 
 use itertools::Itertools;
 use pathfinding::matrix::{
-    directions::{self, DIRECTIONS_8},
+    directions::{self},
     Matrix,
 };
 
@@ -18,25 +18,19 @@ fn is_word_in_direction(
 ) -> bool {
     letters
         .get(*coordinate)
-        .filter(|&&c| {
-            word.starts_with(c)
-        })
-        .and_then(|_| {
-            (word.len() > 1)
-                .then(|| letters.move_in_direction(*coordinate, *direction))
-                .flatten()
-                .map(|next_coord| is_word_in_direction(letters, &next_coord, direction, &word[1..]))
-                .or(Some(true))
-        })
-        .unwrap_or(false)
+        .is_some_and(|&c| word.starts_with(c))
+        && (word.len() == 1
+            || letters
+                .move_in_direction(*coordinate, *direction)
+                .map_or(false, |next_coord| {
+                    is_word_in_direction(letters, &next_coord, direction, &word[1..])
+                }))
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let word = "XMAS";
     let letters = parse_input(input);
 
-    let test = letters.get((0,0));
-    
     let starting_coords: Vec<_> = letters
         .keys()
         .filter(|&(row, column)| letters.get((row, column)) == Some(&'X'))
@@ -52,7 +46,28 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let word = "MAS";
+    let letters = parse_input(input);
+
+    let starting_coords: Vec<_> = letters
+        .keys()
+        .filter(|&(row, column)| letters.get((row, column)) == Some(&'M'))
+        .collect();
+
+    let matches: Vec<_> = starting_coords
+        .iter()
+        .cartesian_product([
+            directions::NE,
+            directions::SE,
+            directions::SW,
+            directions::NW,
+        ])
+        .filter(|&(coord, dir)| is_word_in_direction(&letters, coord, &dir, word))
+        .map(|(&coord, dir)| letters.move_in_direction(coord, dir))
+        .duplicates()
+        .collect();
+
+    Some(matches.len() as u32)
 }
 
 #[cfg(test)]
@@ -67,7 +82,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(9));
     }
 }
