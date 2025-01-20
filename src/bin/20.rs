acb4697167e2586753023b93b5b31cdd2385f92a
Fran Sanchez <fran@mipixel.com>
2025-01-20 17:03:26 +0000

    Improve part 2 speed

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,6 +1,5 @@
-use std::collections::BTreeMap;
+use gxhash::{HashMap, HashSet, HashSetExt};
 
-use itertools::Itertools;
 use pathfinding::{
     matrix::{directions::DIRECTIONS_4, Matrix},
     prelude::dfs,
@@ -10,14 +9,18 @@ advent_of_code::solution!(20);
 
 #[cfg(test)]
 mod constants {
-    pub const MIN_SAVE_PART_1: i32 = 12;
-    pub const MIN_SAVE_PART_2: i32 = 50;
+    pub const HACK_TIME_PART_1: i32 = 2;
+    pub const MIN_SAVING_PART_1: i32 = 12;
+    pub const HACK_TIME_PART_2: i32 = 20;
+    pub const MIN_SAVING_PART_2: i32 = 50;
 }
 
 #[cfg(not(test))]
 mod constants {
-    pub const MIN_SAVE_PART_1: i32 = 100;
-    pub const MIN_SAVE_PART_2: i32 = 100;
+    pub const HACK_TIME_PART_1: i32 = 2;
+    pub const MIN_SAVING_PART_1: i32 = 100;
+    pub const HACK_TIME_PART_2: i32 = 20;
+    pub const MIN_SAVING_PART_2: i32 = 100;
 }
 
 use constants::*;
@@ -26,10 +29,55 @@ fn parse_input(input: &str) -> Matrix<char> {
     Matrix::from_rows(input.lines().map(|l| l.chars())).unwrap()
 }
 
+fn distance(c1: &(usize, usize), c2: &(usize, usize)) -> usize {
+    c1.0.abs_diff(c2.0) + c1.1.abs_diff(c2.1)
+}
+
+fn count_possible_cheats(map: &Matrix<char>, max_hack_time: i32, min_saving: i32) -> usize {
+    let start = map.items().find(|(_, &v)| v == 'S').unwrap().0;
+    let racetrack = dfs(
+        start,
+        |&n| {
+            map.neighbours(n, false)
+                .filter(|&n| map.get(n).unwrap() != &'#')
+        },
+        |&n| map.get(n).unwrap() == &'E',
+    )
+    .unwrap();
+
+    let racetrack_positions: HashMap<&(usize, usize), i32> = racetrack
+        .iter()
+        .enumerate()
+        .map(|(pos, coord)| (coord, pos as i32))
+        .collect();
+
+    let num_cheats = racetrack
+        .iter()
+        .enumerate()
+        .map(|(start_pos, start_coord)| {
+            let mut cheats = HashSet::with_capacity(10000);
+            racetrack
+                .iter()
+                .skip(start_pos + 1)
+                .for_each(|target_coord| {
+                    let distance = distance(start_coord, target_coord) as i32;
+                    if distance <= max_hack_time
+                        && racetrack_positions[target_coord] - (start_pos as i32 + distance)
+                            >= min_saving
+                    {
+                        cheats.insert((start_coord, target_coord));
+                    }
+                });
+            cheats.len()
+        })
+        .sum();
+    num_cheats
+}
+
 pub fn part_one(input: &str) -> Option<usize> {
     let map = parse_input(input);
     let start = map.items().find(|(_, &v)| v == 'S').unwrap().0;
-    let racetrack: BTreeMap<(usize, usize), i32> = dfs(
+    let racetrack: HashMap<(usize, usize), i32> = dfs(
         start,
         |&n| {
             map.neighbours(n, false)
@@ -54,7 +102,7 @@ pub fn part_one(input: &str) -> Option<usize> {
                             .and_then(|cheat_coord| map.move_in_direction(cheat_coord, dir))
                             .filter(|cheat_coord| {
                                 racetrack.get(cheat_coord).map_or(false, |&cheat_pos| {
-                                    cheat_pos - (pos + 2) >= MIN_SAVE_PART_1
+                                    cheat_pos - (pos + HACK_TIME_PART_1) >= MIN_SAVING_PART_1
                                 })
                             })
                     })
@@ -64,53 +112,11 @@ pub fn part_one(input: &str) -> Option<usize> {
     )
 }
 
-fn distance(c1: &(usize, usize), c2: &(usize, usize)) -> usize {
-    c1.0.abs_diff(c2.0) + c1.1.abs_diff(c2.1)
-}
-
 pub fn part_two(input: &str) -> Option<usize> {
     let map = parse_input(input);
-    let start = map.items().find(|(_, &v)| v == 'S').unwrap().0;
-    let racetrack: BTreeMap<(usize, usize), i32> = dfs(
-        start,
-        |&n| {
-            map.neighbours(n, false)
-                .filter(|&n| map.get(n).unwrap() != &'#')
-        },
-        |&n| map.get(n).unwrap() == &'E',
-    )
-    .unwrap()
-    .into_iter()
-    .enumerate()
-    .map(|(pos, coord)| (coord, pos as i32))
-    .collect();
+    let num_cheats = count_possible_cheats(&map, HACK_TIME_PART_2, MIN_SAVING_PART_2);
 
-    let sorted_racetrack = racetrack.iter().sorted_by_key(|(_coord, &pos)| pos);
-
-    Some(
-        sorted_racetrack
-            .clone()
-            .enumerate()
-            .flat_map(|(i, (&coord, &pos))| {
-                // this could be optimised by checking only elements in the racetrack from current to end
-                sorted_racetrack
-                    .clone()
-                    .skip(i)
-                    .filter_map(|(cheat_coord, &cheat_pos)| {
-                        let hack_duration = distance(&coord, cheat_coord) as i32;
-                        if cheat_pos > pos
-                            && hack_duration <= 20
-                            && cheat_pos - (pos + hack_duration) >= MIN_SAVE_PART_2
-                        {
-                            return Some((coord, cheat_coord));
-                        }
-                        None
-                    })
-                    .unique()
-                    .collect::<Vec<_>>()
-            })
-            .count(),
-    )
+    Some(num_cheats)
 }
 
 #[cfg(test)]
